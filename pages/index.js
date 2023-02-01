266d33a0ba1fb013371c9ccb90c2d6fb0fee830d
lecongtu <tulecong24@gmail.com>
2023-02-01 11:16:30 +0700

    service section

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,13 @@ import React from 'react'
 
 // INTERNAL IMPORT
 import Style from '../styles/Home.module.css'
-import { HeroSection } from '../components'
+import { HeroSection, Service } from '../components'
 
 const Home = () => {
   return (
     <div className={Style.homePage}>
       <HeroSection />
+      <Service />
     </div>
   )
 }
